8c106dfe538d46722bc7078f8fd6f222110f9085
Marcin Płaczek <marcin@marcinplaczek.com>
2023-09-28 17:40:45 +0200

    Replace favicon with SP9MPZ logo + adjust its size

--- src/components/common/Logo.tsx
+++ src/components/common/Logo.tsx
@@ -1,18 +1,18 @@
 import { component$ } from "@builder.io/qwik";
 
 // @ts-ignore
-import logoSrc from "~/assets/images/logo.png?width=64&height=64&png";
+import logoSrc from "~/assets/images/logo.svg?width=96&height=96&svg";
 
 export default component$(() => (
   <span class="self-center ml-2 text-2xl md:text-xl font-bold text-gray-900 whitespace-nowrap dark:text-white flex items-center">
     <img
       src={logoSrc}
       class="inline-block mr-1"
-      width={32}
-      height={32}
-      alt="Qwind Logo"
+      width={48}
+      height={48}
+      alt="SP9MPZ logo"
       loading="lazy"
     />
-    Qwind
+    SP9MPZ
   </span>
 ));
