38f1c775dbd896f118c5d2633765d63e7fbc6c14
Marcin Płaczek <marcin@marcinplaczek.com>
2023-09-28 17:43:20 +0200

    Remove not needed styling that caused bad styling of header when scrolling up the the top

--- src/components/widgets/Header.tsx
+++ src/components/widgets/Header.tsx
@@ -14,19 +14,8 @@ export default component$(() => {
 
   return (
     <header
-      class={`sticky top-0 z-40 flex-none mx-auto w-full transition-all${
-        store.isScrolling
-          ? " md:bg-white/90 md:backdrop-blur-sm dark:md:bg-slate-900/90 bg-white dark:bg-slate-900"
-          : ""
-      }`}
+      class={`sticky top-0 z-40 flex-none mx-auto w-full transition-all md:bg-white/90 md:backdrop-blur-sm dark:md:bg-slate-900/90 bg-white dark:bg-slate-900`}
       id="header"
-      window:onScroll$={() => {
-        if (!store.isScrolling && window.scrollY >= 10) {
-          store.isScrolling = true;
-        } else if (store.isScrolling && window.scrollY < 10) {
-          store.isScrolling = false;
-        }
-      }}
     >
       <div class="py-3 px-3 mx-auto w-full md:flex md:justify-between max-w-7xl md:px-4">
         <div class="flex justify-between">
